4b44e647f1b54c3ce8c11e7ec1bec76e82bf68dc
Joe O'Regan <k00203642@student.lit.ie>
2017-11-17 03:20:59 +0000

    Asynchronous Client: tidied and commented

--- Asynchronous Client/client.c
+++ Asynchronous Client/client.c
@@ -3,77 +3,59 @@
 
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdio.h>										// printf()
+#include <stdio.h>											// printf()
 #include <errno.h>
-#include <string.h>										// 23/09/2017 Warning for strcpy, bzero()
-#include <stdlib.h>										// 23/09/2017 Warning for exit
-#include <unistd.h>										// 23/09/2017 gethostname(), write(), read(), close()
-#include <arpa/inet.h>										// 23/09/2017 inet_ntop()
+#include <string.h>											// 23/09/2017 Warning for strcpy, bzero()
+#include <stdlib.h>											// 23/09/2017 Warning for exit
+#include <unistd.h>											// 23/09/2017 gethostname(), write(), read(), close()
+#include <arpa/inet.h>											// 23/09/2017 inet_ntop()
 #include "../HandleErrors.h"
 #include "../Hangman.h"
+#include "../DrawHangman.h"
 #include "../CreateTCPSocket.h"
 	
 void str_cli(FILE *fp, int sockfd);
 
 int main(int argc, char **argv) {
-	int sockfd;
-	struct sockaddr_in servaddr = createTCPClientSocket(&sockfd, (argc == 1) ?  SRV_IP : argv[1]);
-/*
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(TCP_PORT_NUM);
-	inet_pton(AF_INET, (argc == 1) ?  SRV_IP : argv[1], &servaddr.sin_addr);
-
-	connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-
-	printf("Client Connecting On Port: %d \n", ntohs(servaddr.sin_port));			// Display port number
-*/
-
-//	str_cli(stdin, sockfd);									// do it all
-
-	//exit(0);
-//}
-
-//void str_cli(FILE *fp, int sockfd) {
-	int maxfdp1, stdineof, count;
+	int sockfd, maxfdp1, stdineof, count;
 	fd_set rset;
+
+	struct sockaddr_in servaddr = createTCPClientSocket(&sockfd, (argc == 1) ?  SRV_IP : argv[1]);	// Make the socket connection, with default localhost, or entered IP address
 	char buf[LINESIZE];
-
 	stdineof = 0;
-	FD_ZERO(&rset);
+	FD_ZERO(&rset);											// Clear the set
 
+	drawHangman();											// Display game logo
+	
+	count = read (sockfd, buf, LINESIZE);								// Read 1st (empty) part word from server
+	write (1, buf, count);										// Display on screen
 
-	count = read (sockfd, buf, LINESIZE);							// Read from server
-	write (1, buf, count);									// Display on screen
-
-	for ( ; ; ) {										// loop
-		if (stdineof == 0) FD_SET(fileno(stdin), &rset);
-		FD_SET(sockfd, &rset);
+	for ( ; ; ) {											// Guess loop
+		if (stdineof == 0) FD_SET(fileno(stdin), &rset);					// FD_SET - add stdin to file descriptor list
+		FD_SET(sockfd, &rset);									// Add socket to file descriptor list
 		maxfdp1 = max(fileno(stdin), sockfd) + 1;
 		select(maxfdp1, &rset, NULL, NULL, NULL);
 
-		if (FD_ISSET(sockfd, &rset)) {							// socket is readable
-			if ( (count = read(sockfd, buf, LINESIZE)) == 0) {			// Read from socket
-				//if (stdineof == 1) return;					// normal termination
-				if (stdineof == 1) break;					// normal termination
+		if (FD_ISSET(sockfd, &rset)) {								// socket is readable, FD_ISSET = check socket is in file descriptor list
+			if ( (count = read(sockfd, buf, LINESIZE)) == 0) {				// Get input from socket
+				if (stdineof == 1) break;						// normal termination, EOF already received
 				else displayErrMsg("str_cli: server terminated prematurely");
 			}
 
-			write(fileno(stdout), buf, count);					// Write to screen
+			write(fileno(stdout), buf, count);						// Write to screen
 		}
 
-		if (FD_ISSET(fileno(stdin), &rset)) {  						// input is readable
-			if ( (count = read(fileno(stdin), buf, LINESIZE)) == 0) {
-				stdineof = 1;
-				shutdown(sockfd, SHUT_WR);					// send FIN
-				FD_CLR(fileno(stdin), &rset);
-				continue;
+		if (FD_ISSET(fileno(stdin), &rset)) {  							// input is readable, check standard input is in list of file descriptors
+			if ( (count = read(fileno(stdin), buf, LINESIZE)) == 0) {			// Get input from keyboard, and if none
+				stdineof = 1;								// EOF received, set stdineof flag, call shutdown, and send FIN
+				shutdown(sockfd, SHUT_WR);						// send FIN, SHUT_WR = close write half of connection
+				FD_CLR(fileno(stdin), &rset);						// Remove standard input from the file descriptors list
+				continue;								// Skip the write to server socket, and loop again
 			}
 
-			write(sockfd, buf, count);						// Send to socket
+			write(sockfd, buf, count);							// Otherwise, Send input to socket
 		}
 	}
-	exit(0);
+
+	exit(0);											// Terminate the program, terminating open descriptors
 }
