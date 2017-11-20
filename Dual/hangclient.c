464014acac5c46525e5278b1f5efcb8dcc192809
jasfoley <32104040+jasfoley@users.noreply.github.com>
2017-11-20 17:41:32 +0000

    Update hangclient.c

--- Dual/hangclient.c
+++ Dual/hangclient.c
@@ -5,6 +5,9 @@
  #include <sys/socket.h>
  #include <netinet/in.h>
  #include <netdb.h>
+ #include <stdllib.h>
+ #include <string.h>
+ #include <unistd.h> // read(), write()
 
  # define LINESIZE 80
  # define HANGMAN_TCP_PORT 1066
@@ -31,7 +34,7 @@
 
  	host_info = gethostbyname(server_name);
  	if (host_info == NULL) {
- 		fprintf (stderr, "%s: unknown host:%s \ n", argv [0], server_name);
+ 		fprintf (stderr, "%s: unknown host: %s \n", argv [0], server_name);
  		exit (2);
  	}
 
@@ -49,7 +52,7 @@
  Take a line from the server and show it, take a line and send the user input to the server. 
  Repeat until the server terminates the connection. */
 
- 	printf ("Connected to server% s \ n", server_name);
+ 	printf ("Connected to server %s \n", server_name);
  	while ((count = read (sock, i_line, LINESIZE)) > 0) {
  		write (1, i_line, count);
  		count = read (0, o_line, LINESIZE);//0 = STDIN
